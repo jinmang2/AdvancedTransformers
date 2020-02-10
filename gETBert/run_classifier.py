1a4586755a40b6118b8fccc8f6be5e4b29b2c065
MyungHoon Jin <jinmang2@naver.com>
2020-02-10 19:16:25 +0900

    금일 작업 내용 추가

--- gETBert/run_classifier.py
+++ gETBert/run_classifier.py
@@ -216,41 +216,7 @@ def convert_tokens_to_ids(vocab, tokens):
 def convert_ids_to_tokens(inv_vocab, ids):
     return convert_by_vocab(inv_vocab, ids)
 
-def convert_to_unicode(text):
-    if six.PY3:
-        if isinstance(text, str):
-            return text
-        elif isinstance(text, bytes):
-            return text.decode("utf-8", "ignore")
-        else:
-            raise ValueError("Unsupported string type: %s" % (type(text)))
-    elif six.PY2:
-        if isinstance(text, str):
-            return text.decode("utf-8", "ignore")
-        elif isinstance(text, unicode):
-            return text
-        else:
-            raise ValueError("Unsupported string type: %s" % (type(text)))
-    else:
-        raise ValueError("Not running on Python2 or Python 3?")
 
-def printable_text(text):
-    if six.PY3:
-        if isinstance(text, str):
-            return text
-        elif isinstance(text, bytes):
-            return text.decode("utf-8", "ignore")
-        else:
-            raise ValueError("Unsupported string type: %s" % (type(text)))
-    elif six.PY2:
-        if isinstance(text, str):
-            return text
-        elif isinstance(text, unicode):
-            return text.encode("utf-8")
-        else:
-            raise ValueError("Unsupported string type: %s" % (type(text)))
-    else:
-        raise ValueError("Not running on Python2 or Python 3?")
 
 def create_model(bert_config, is_training, input_ids, input_mask, segment_ids,
                  labels, num_labels, use_one_hot_embeddings):
@@ -420,7 +386,7 @@ def input_fn_builder(features, seq_length, is_training, drop_remainder):
 
 # This function is not used by this file but is still used by the Colab and
 # people who depend on it.
-def convert_examples_to_features(examples, label_list, max_seq_length,
+def examples_to_features(examples, label_list, max_seq_length,
                                  tokenizer):
     features = []
     for (ex_index, example) in enumerate(examples):
@@ -430,3 +396,200 @@ def convert_examples_to_features(examples, label_list, max_seq_length,
                                          max_seq_length, tokenizer)
         features.append(feature)
     return features
+
+def file_based_convert_examples_to_features(examples,
+                                            label_list,
+                                            max_seq_length,
+                                            tokenizer,
+                                            openapi_key,
+                                            output_file):
+    writer = tf.python_io.TFRecordWriter(output_file)
+    komoran = Komoran()
+    for (ex_index, example) in enumerate(examples):
+        if ex_index % 10000 == 0:
+            tf.logging.info("Writing example %d of %d" %
+                            (ex_index, len(examples)))
+        feature = convert_single_example(ex_index, example,
+                                         label_list,
+                                         max_seq_length,
+                                         tokenizer,
+                                         openapi_key,
+                                         komoran=komoran)
+
+        def create_int_feature(values):
+            f = tf.train.Feature(int64_list=tf.train.Int64List(value=list(values)))
+            return f
+
+        features = collections.OrderedDict()
+        features["input_ids"] = create_int_feature(feature.input_ids)
+        features["input_mask"] = create_int_feature(feature.input_mask)
+        features["segment_ids"] = create_int_feature(feature.segment_ids)
+        features["label_ids"] = create_int_feature([feature.label_id])
+        features["is_real_example"] = create_int_feature([int(feature.is_real_example)])
+
+        tf_example = tf.train.Example(features=tf.train.Features(feature=features))
+        writer.write(tf_example.SerializeToString())
+    writer.close()
+
+def convert_single_example(ex_index, example,
+                           label_list,
+                           max_seq_length,
+                           tokenizer,
+                           openapi_key,
+                           komoran=None):
+    if isinstance(example, PaddingInputExample):
+        return InputFeatures(
+            input_ids=[0] * max_seq_length,
+            input_mask=[0] * max_seq_length,
+            segment_ids=[0] * max_seq_length,
+            label_id=0,
+            is_real_example=False)
+
+    label_map = {label : i for (i, label) in enumerate(label_list)}
+
+    if openapi_key is None:
+        tokens_a = ' '.join(
+            [i[0] + '/' + i[1]
+             for i in komoran.pos(example.text_a)])
+    else:
+        tokens_a = do_lang(openapi_key, example.text_a)
+#     if "openapi error" in tokens_a:
+#         tf.logging.info("(%d--%s)" % (ex_index, tokens_a))
+    tokens_a = tokenizer.tokenize(tokens_a)
+
+    if example.text_b:
+        tokens_b = do_lang(openapi_key, example.text_b)
+        if "openapi error" in tokens_a:
+            tf.logging.info("(%d--%s)" % (ex_index, tokens_b))
+            tokens_b = ' '.join(
+                [i[0] + '/' + i[1]
+                 for i in Komoran().pos(example.text_b)])
+        tokens_b = tokenizer.tokenize(tokens_b)
+
+        def _truncate_seq_pair(tokens_a, tokens_b, max_length):
+            while True:
+                total_length = len(tokens_a) + len(tokens_b)
+                if total_length <= max_length:
+                    break
+                if len(tokens_a) > len(tokens_b):
+                    tokens_a.pop()
+                else:
+                    tokens_b.pop()
+        # Account for [CLS], [SEP],and [SEP] with "-3"
+        _truncate_seq_pair(tokens_a, tokens_b, max_seq_length-3)
+    else:
+        # Account for [CLS] and [SEP] with "-2"
+        if len(tokens_a) > max_seq_length-2:
+            tokens_a = tokens_a[:(max_seq_length-2)]
+
+    # The convention in BERT is:
+    # (a) For sequence pairs:
+    # tokens:   [CLS] is this jack ##son ##ville ? [SEP] no it is not . [SEP]
+    # type_ids: 0     0  0    0    0     0       0 0     1  1  1  1   1 1
+    # (b) For single sequences:
+    # tokens:   [CLS] the dog is hairy . [SEP]
+    # type_ids: 0     0   0   0  0     0 0
+    #
+    # Where "type_ids" are used to indicate whether this is the first
+    # sequence or the second sequence. The embedding vectors for 'type=0' and
+    # 'type=1' were learned during pre-training and are added to the wordpiece
+    # embedding vector (and position vector). This is not "strictly" necessary
+    # since the [SEP] token unambigiously separates the sequences, but it makes
+    # if easier for the model to learn the concept of sequences.
+    #
+    # For classification tasks, the first vector (corresponding to [CLS]) is
+    # used as the "sentence vector". Note that this only makes sense because
+    # the entire model is fine-tuned.
+    tokens = ["[CLS]"] + tokens_a + ["[SEP]"]
+    segment_ids = [0] * len(tokens)
+
+    # tokens_b = None
+    # if tokens_b:
+    if example.text_b:
+        tokens += tokens_b + ["[SEP]"]
+        segment_ids += [1] * (len(tokens_b) + 1)
+
+    input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+    # The mask has 1 for real tokens and 0 for padding tokens. Only real
+    # tokens are attended to.
+    input_mask = [1] * len(input_ids)
+
+    # Zero-pad up to the sequence length.
+    padding = [0] * (max_seq_length - len(input_ids))
+    input_ids += padding
+    input_mask += padding
+    segment_ids += padding
+
+    assert len(input_ids) == max_seq_length
+    assert len(input_mask) == max_seq_length
+    assert len(segment_ids) == max_seq_length
+
+    label_id = label_map[example.label]
+
+    if ex_index < 5:
+        tf.logging.info("*** Example ***")
+        tf.logging.info("guid: %s" % (example.guid))
+        tf.logging.info("tokens: %s" % " ".join(
+                [str(x) for x in tokens]))
+        tf.logging.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
+        tf.logging.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
+        tf.logging.info(
+                "segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
+        tf.logging.info("label: %s (id = %d)" % (example.label, label_id))
+
+    feature = InputFeatures(
+        input_ids=input_ids,
+        input_mask=input_mask,
+        segment_ids=segment_ids,
+        label_id=label_id,
+        is_real_example=True)
+
+    return feature
+
+def file_based_input_fn_builder(input_file, seq_length, is_training,
+                                drop_remainder, use_tpu=False):
+    """Creates an 'input_fn' closure to be passed to TPUEstimator."""
+
+    name_to_features = {
+        'input_ids': tf.FixedLenFeature([seq_length], tf.int64),
+        'input_mask': tf.FixedLenFeature([seq_length], tf.int64),
+        'segment_ids': tf.FixedLenFeature([seq_length], tf.int64),
+        'label_ids': tf.FixedLenFeature([], tf.int64),
+        'is_real_example': tf.FixedLenFeature([], tf.int64),
+    }
+
+    def _decode_record(record, name_to_features, use_tpu=use_tpu):
+        example = tf.parse_single_example(record, name_to_features)
+
+        # tf.Example only supports tf.int64, but the TPU only supports tf.int32.
+        # So cast all int64 to int32.
+        # But in this time, we use GPU.
+        if use_tpu:
+            for name in list(example.keys()):
+                t = example[name]
+                if t.dtype == tf.int64:
+                    t = tf.to_int32(t)
+                example[name] = t
+
+        return example
+
+    def input_fn(params):
+        batch_size = params['batch_size']
+
+        # For training, we want to lot of parallel reading and shuffling.
+        # For eval, we want no shuffling and parallel reading doesn't matter.
+        d = tf.data.TFRecordDataset(input_file)
+        if is_training:
+            d = d.repeat()
+            d = d.shuffle(buffer_size=100)
+
+        d = d.apply(
+            tf.contrib.data.map_and_batch(
+                lambda record: _decode_record(record, name_to_features),
+                batch_size=batch_size,
+                drop_remainder=drop_remainder))
+
+        return d
+
+    return input_fn
